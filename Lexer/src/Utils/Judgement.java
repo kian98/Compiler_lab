3e0024a22fc5ca781984afd74ad88f98e911381d
Kian <1207529462@qq.com>
2018-05-20 12:05:51 +0800

    Add files via upload

--- Lexer/src/Utils/Judgement.java
+++ Lexer/src/Utils/Judgement.java
@@ -7,12 +7,55 @@ public class Judgement {
     private String[] keywords;
     private String[] type;
 
-    Judgement() {
-        keywords = new String[]{"do", "end", "for", "if", "printf", "scanf", "then", "while"};
+    public Judgement() {
+        keywords = new String[]{"include", "do", "end", "for", "if", "printf", "scanf", "then", "while"};
         relationOperators = new String[]{"<", "<=", "=", ">", ">=", "<>"};
         arithmeticOperators = new String[]{"+", "-", "*", "/", "++", "--"};
-        separators = new String[]{",", ";", "(", ")", "{", "}", "\"","#"};
-        type = new String[]{"关键字", "分界符", "算术运算符", "关系运算符", "常数", "标识符", "ERROR"};
+        separators = new String[]{",", ";", "(", ")", "{", "}", "\"", "#"};
+        type = new String[]{"关键词", "分界符", "算术运算符", "关系运算符", "常数", "标识符", "ERROR"};
+    }
+
+    public boolean addNewValue(int index, String newValue) {
+        String[] valuesTemp;
+        switch (index) {
+            case 0:
+                if (!isKeyword(newValue)) {
+                    valuesTemp = new String[keywords.length + 1];
+                    System.arraycopy(keywords, 0, valuesTemp, 0, keywords.length);
+                    valuesTemp[keywords.length] = newValue;
+                    keywords = valuesTemp;
+                    return true;
+                }
+                break;
+            case 1:
+                if (!isSeparator(newValue)) {
+                    valuesTemp = new String[separators.length + 1];
+                    System.arraycopy(separators, 0, valuesTemp, 0, separators.length);
+                    valuesTemp[separators.length] = newValue;
+                    separators = valuesTemp;
+                    return true;
+                }
+                break;
+            case 2:
+                if (!isRelationOperator(newValue)) {
+                    valuesTemp = new String[relationOperators.length + 1];
+                    System.arraycopy(relationOperators, 0, valuesTemp, 0, relationOperators.length);
+                    valuesTemp[relationOperators.length] = newValue;
+                    relationOperators = valuesTemp;
+                    return true;
+                }
+                break;
+            case 3:
+                if (!isArithmeticOperator(newValue)) {
+                    valuesTemp = new String[arithmeticOperators.length + 1];
+                    System.arraycopy(arithmeticOperators, 0, valuesTemp, 0, arithmeticOperators.length);
+                    valuesTemp[arithmeticOperators.length] = newValue;
+                    arithmeticOperators = valuesTemp;
+                    return true;
+                }
+                break;
+        }
+        return false;
     }
 
     public boolean isKeyword(String str) {
